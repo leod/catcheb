37a6402608da8b6fd1c0a9ee769a208707b854c0
leod <subtle.frustration@proton.me>
2020-05-02 14:06:12 +0200

    Clean up WebRTC client

--- clnt/src/webrtc.rs
+++ clnt/src/webrtc.rs
@@ -10,12 +10,12 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{
-    RtcConfiguration, RtcDataChannel, RtcDataChannelInit, RtcPeerConnection,
+    MessageEvent, RtcConfiguration, RtcDataChannel, RtcDataChannelInit, RtcPeerConnection,
     RtcSessionDescriptionInit,
 };
 
 #[derive(Debug, Clone)]
-pub enum ConnectionError {
+pub enum ConnectError {
     NewRtcPeerConnection(JsValue),
     CreateOffer(JsValue),
     SetLocalDescription(JsValue),
@@ -51,100 +51,103 @@ impl Default for Config {
 pub struct Client {}
 
 impl Client {
-    pub async fn connect(config: Config) -> Result<Self, ConnectionError> {
+    pub async fn connect(config: Config) -> Result<Self, ConnectError> {
         info!("Establishing WebRTC connection");
 
-        let ice_servers: JsValue = {
-            let json = "[{\"urls\":[".to_string()
-                + &config
-                    .ice_server_urls
-                    .iter()
-                    .map(|url| "\"".to_string() + url + "\"")
-                    .collect::<Vec<_>>()
-                    .join(",")
-                + "]}]";
-            JSON::parse(&json).unwrap()
-        };
-
-        info!("WebRTC ICE servers: {:?}", ice_servers);
-
-        let rtc_configuration: RtcConfiguration = {
-            let mut v = RtcConfiguration::new();
-            v.ice_servers(&ice_servers);
-            v
-        };
-
-        let peer: RtcPeerConnection = RtcPeerConnection::new_with_configuration(&rtc_configuration)
-            .map_err(ConnectionError::NewRtcPeerConnection)?;
-
-        let data_channel_init: RtcDataChannelInit = {
-            let mut v = RtcDataChannelInit::new();
-            v.ordered(false).max_retransmits(0);
-            v
-        };
-
-        let channel: RtcDataChannel =
-            peer.create_data_channel_with_data_channel_dict("webudp", &data_channel_init);
+        let peer: RtcPeerConnection = new_rtc_peer_connection(&config)?;
+        let channel: RtcDataChannel = create_data_channel(&peer);
 
         let offer: RtcSessionDescriptionInit = JsFuture::from(peer.create_offer())
             .await
-            .map_err(ConnectionError::CreateOffer)?
+            .map_err(ConnectError::CreateOffer)?
             .into();
 
         JsFuture::from(peer.set_local_description(&offer))
             .await
-            .map_err(ConnectionError::SetLocalDescription)?;
+            .map_err(ConnectError::SetLocalDescription)?;
 
-        // Request a session from the WebRTC server
-        let reply: JsValue = {
-            let mut opts = web_sys::RequestInit::new();
-            opts.method("POST");
-            opts.mode(web_sys::RequestMode::SameOrigin);
-            opts.body(Some(
-                &Reflect::get(&offer, &JsValue::from_str("sdp")).unwrap(),
-            ));
-
-            info!("Requesting WebRTC session...");
-
-            let request = web_sys::Request::new_with_str_and_init(&config.address, &opts)
-                .map_err(ConnectionError::NewRequest)?;
-
-            let window = web_sys::window().unwrap();
-            let response_value: JsValue = JsFuture::from(window.fetch_with_request(&request))
-                .await
-                .map_err(ConnectionError::Fetch)?;
-            assert!(response_value.is_instance_of::<web_sys::Response>());
-            let response: web_sys::Response = response_value.dyn_into().unwrap();
-
-            if (response.status() != 200) {
-                return Err(ConnectionError::ResponseStatus(response.status()));
-            }
-
-            let reply: JsValue =
-                JsFuture::from(response.json().map_err(ConnectionError::ResponseJson)?)
-                    .await
-                    .map_err(ConnectionError::ResponseJson)?;
-
-            info!("Reply: {:?}", reply);
-
-            reply
-        };
+        info!("Requesting WebRTC session...");
+        let reply: JsValue = request_session(&config.address, &offer).await?;
+        info!("Reply: {:?}", reply);
 
         let (answer, candidate) = (
             Reflect::get(&reply, &JsValue::from_str("answer"))
-                .map_err(ConnectionError::ResponseJson)?,
+                .map_err(ConnectError::ResponseJson)?,
             Reflect::get(&reply, &JsValue::from_str("candidate"))
-                .map_err(ConnectionError::ResponseJson)?,
+                .map_err(ConnectError::ResponseJson)?,
         );
 
         JsFuture::from(peer.set_remote_description(&answer.into()))
             .await
-            .map_err(ConnectionError::SetRemoteDescription)?;
+            .map_err(ConnectError::SetRemoteDescription)?;
 
         JsFuture::from(peer.add_ice_candidate_with_opt_rtc_ice_candidate(Some(&candidate.into())))
             .await
-            .map_err(ConnectionError::AddIceCandidate)?;
+            .map_err(ConnectError::AddIceCandidate)?;
 
         Ok(Client {})
     }
+
+    fn on_open(event: &MessageEvent) {
+        info!("Connection has been established");
+    }
+}
+
+fn new_rtc_peer_connection(config: &Config) -> Result<RtcPeerConnection, ConnectError> {
+    let ice_servers: JsValue = {
+        let json = "[{\"urls\":[".to_string()
+            + &config
+                .ice_server_urls
+                .iter()
+                .map(|url| "\"".to_string() + url + "\"")
+                .collect::<Vec<_>>()
+                .join(",")
+            + "]}]";
+        JSON::parse(&json).unwrap()
+    };
+
+    info!("WebRTC ICE servers: {:?}", ice_servers);
+
+    let mut rtc_configuration = RtcConfiguration::new();
+    rtc_configuration.ice_servers(&ice_servers);
+
+    RtcPeerConnection::new_with_configuration(&rtc_configuration)
+        .map_err(ConnectError::NewRtcPeerConnection)
+}
+
+fn create_data_channel(peer: &RtcPeerConnection) -> RtcDataChannel {
+    let mut data_channel_init: RtcDataChannelInit = RtcDataChannelInit::new();
+    data_channel_init.ordered(false).max_retransmits(0);
+
+    peer.create_data_channel_with_data_channel_dict("webudp", &data_channel_init)
+}
+
+async fn request_session(
+    address: &str,
+    offer: &RtcSessionDescriptionInit,
+) -> Result<JsValue, ConnectError> {
+    let mut opts = web_sys::RequestInit::new();
+    opts.method("POST");
+    opts.mode(web_sys::RequestMode::SameOrigin);
+    opts.body(Some(
+        &Reflect::get(&offer, &JsValue::from_str("sdp")).unwrap(),
+    ));
+
+    let request = web_sys::Request::new_with_str_and_init(&address, &opts)
+        .map_err(ConnectError::NewRequest)?;
+
+    let window = web_sys::window().unwrap();
+    let response_value: JsValue = JsFuture::from(window.fetch_with_request(&request))
+        .await
+        .map_err(ConnectError::Fetch)?;
+    assert!(response_value.is_instance_of::<web_sys::Response>());
+    let response: web_sys::Response = response_value.dyn_into().unwrap();
+
+    if response.status() != 200 {
+        return Err(ConnectError::ResponseStatus(response.status()));
+    }
+
+    JsFuture::from(response.json().map_err(ConnectError::ResponseJson)?)
+        .await
+        .map_err(ConnectError::ResponseJson)
 }
